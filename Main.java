f836b913dcc679fc48d6cabbeeaaeab41a94e550
Shubham sharma <sharma08shubham@gmail.com>
2025-01-14 17:58:33 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+package jenkins_practice;
+import java.util.Date;
+
+class Main{
+
+    public static void main(String[] args) {
+        
+        System.out.println("This is a java program");
+        System.out.println("This is the output from java program0");
+        System.out.println(new Date());
+    }
+}
